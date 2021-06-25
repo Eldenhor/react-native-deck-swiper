29359ce3a32724d89b9b57edf0dfa713859d1cfd
Radik Khammatov <48619068+Eldenhor@users.noreply.github.com>
2021-06-25 19:37:26 +0300

    Update Swiper.js
    fixed wrong cards start position

--- Swiper.js
+++ Swiper.js
@@ -212,7 +212,9 @@ class Swiper extends Component {
     if (!this.state.panResponderLocked) {
       this.state.pan.setOffset({
         x: this._animatedValueX,
-        y: this._animatedValueY
+        y: this._animatedValueY,
+        x: 0,
+        y: 0
       })
     }
 
